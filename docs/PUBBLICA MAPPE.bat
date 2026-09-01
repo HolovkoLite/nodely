1787ee654245d388f5f650af0e79d7eea1aa10e4
HolovkoLite <sir.holovko@gmail.com>
2026-09-01 14:52:10 +0200

    Aggiornamento automatico mappe - 2026-09-01 14:52:10

---
+++ docs/PUBBLICA MAPPE.bat
@@ -0,0 +1,10 @@
+@echo off
+title Pubblicazione Mappe Nodely
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "C:\Users\tomma\Documents\Varie\Mappe Nodely\pubblica-mappe.ps1"
+
+echo.
+echo ------------------------------------------------------------
+echo Premi un tasto per chiudere questa finestra.
+echo ------------------------------------------------------------
+pause >nul
